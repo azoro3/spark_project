300818ff9aeef33d4372fb5bc89bfdd051ce8a3b
ARTHUR-PC\Arthur <arthur.rozo@outlook.fr>
2018-03-15 16:38:54 +0100

    rapport

--- Q4.scala
+++ Q4.scala
@@ -5,10 +5,10 @@ val splittedData = data.map(line => {
     val splittedArrayLength = splitedLine.length
     (splitedLine(splittedArrayLength-15),splitedLine(splittedArrayLength-4),splitedLine(splittedArrayLength-3))
     })
-val filteredData = splittedData.filter(t => (t._1.equals("true")||t._1.equals("false")))
+val filteredData = splittedData.filter(t => (t._1.equals("true")))
 val mappedData = filteredData.map(line => (line,1)).reduceByKey(_+_)
 mappedData.saveAsTextFile("Z:/Doc/Cours/progDev/Perrin/out_question4")
 print("Q4: ")
 print(System.currentTimeMillis - debut4)
 print(" ms")
-Q4:23866 ms
+Q4:55500 ms
